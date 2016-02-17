3a05938962d8fa4995c91c69c6a34c71c33936ae
Wesley <wesley.nmtorres@gmail.com>
2016-02-17 20:30:18 -0300

    integração com ionic material

--- sistemauto/www/js/app.js
+++ sistemauto/www/js/app.js
@@ -8,35 +8,52 @@
 
     var app = angular.module('sistemauto', [
         'ionic',
+        'ngCordova',
         'ionic-material',
         'sistemauto.service',
-        'sistemauto.controller',
+        'sistemauto.controller'
     ]);
+
+    app.run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
+
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    });
 
 
     app.config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('login', {
                 url: '/login',
-                templateUrl: '../templates/login.html',
-                
+                templateUrl: 'templates/login.html'
             })
-        .state('app', {
-            url: '/app',
-            abstract: true,
-            templateUrl: '../templates/menu.html'
-        })
-         .state('app.home', {
-            url: '/home',
-            views: {
-                'menuContent': {
-                    templateUrl: 'templates/login.html'                    
+            .state('app', {
+                url: '',
+                abstract: true,
+                templateUrl: 'templates/menu.html'
+            })
+            .state('app.home', {
+                url: '/home',
+                views: {
+                    'menuContent': {
+                        template: '<b>teste</b>'
+                    }
                 }
-            }
-        });
+            });
 
         $urlRouterProvider.otherwise('/home');
     });
 
 
-}());
+})();
