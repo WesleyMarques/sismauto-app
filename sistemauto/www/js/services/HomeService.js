38239fdec50822aa409824b2d541cd966af2413e
Wesley <wesley.nmtorres@gmail.com>
2016-02-17 21:39:35 -0300

    estrutura pronta, mas sem token de acesso ao servidor

--- sistemauto/www/js/services/HomeService.js
+++ sistemauto/www/js/services/HomeService.js
@@ -6,11 +6,24 @@
      *
      * Description
      */
-    angular.module('sistemauto.service').controller('homeService', HomeController);
+    angular.module('sistemauto.service').controller('autoEscolaService', autoEscolaService);
 
-    
+    autoEscolaService.$injector = ['$q', '$http'];
 
-    function HomeController() {
+    function autoEscolaService($q, $http) {
         var self = this;
+
+        self.getAll = function(){
+            var deffered = $q.defer();
+            $http.get("").then(
+                function(data){
+
+                },function(reason){
+
+                });
+            return deffered.promise;
+        };
+
+        return self;
     };
 }());
