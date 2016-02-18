3b3063c2d802848c492a2bd457ee8cb0ab2d4f4d
Wesley <wesley.torres@ccc.ufcg.edu.br>
2016-02-18 16:58:12 -0300

    versão final para primeira sprint

--- sistemauto/www/js/controllers/HomeController.js
+++ sistemauto/www/js/controllers/HomeController.js
@@ -8,9 +8,32 @@
      */
     angular.module('sistemauto.controller').controller('HomeController', HomeController);
 
-    
 
-    function HomeController() {
+
+    HomeController.$injector = ['autoEscolaService', '$scope'];
+
+
+    function HomeController(autoEscolaService, $scope) {
         var self = this;
+
+        self.autoEscolas = [];
+
+        $scope.$on('$ionicView.beforeEnter', function() {
+            self.loadAll();
+        })
+
+        self.loadAll = function() {
+            autoEscolaService.getAll().then(
+                function(data) {
+                    console.log(data);
+                    self.autoEscolas = data;
+                },
+                function(reason) {
+                    console.log(reason);
+                });
+        };
+
+
     };
+
 }());
--- sistemauto/www/js/services/HomeService.js
+++ sistemauto/www/js/services/HomeService.js
@@ -6,11 +6,25 @@
      *
      * Description
      */
-    angular.module('sistemauto.service').controller('homeService', HomeController);
+    angular.module('sistemauto.service').service('autoEscolaService', autoEscolaService);
 
-    
+    autoEscolaService.$injector = ['$q', '$http'];
 
-    function HomeController() {
+    function autoEscolaService($q, $http) {
         var self = this;
+
+        self.getAll = function() {
+            var deffered = $q.defer();
+            $http.get("http://sistemauto.herokuapp.com/api/user").then(
+                function(data) {
+                    return deffered.resolve(data.data);
+                },
+                function(reason) {
+                    return deffered.reject(reason);
+                });
+            return deffered.promise;
+        };
+
+        return self;
     };
 }());
