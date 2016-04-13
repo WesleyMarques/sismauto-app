b0788e4f8f4e0612dfc3762f710546d7ed924ae6
Wesley <wesley.nmtorres@gmail.com>
2016-04-13 20:20:30 -0300

    lista das auto escolas com filtro

--- sistemauto/www/js/controllers/HomeController.js
+++ sistemauto/www/js/controllers/HomeController.js
@@ -8,9 +8,23 @@
      */
     angular.module('sistemauto.controller').controller('HomeController', HomeController);
 
+    HomeController.$injector = ['autoEscolaService'];
     
 
-    function HomeController() {
+    function HomeController(autoEscolaService) {
         var self = this;
+
+        self.autoEscolas = [];
+
+        self.loadAll = function(){
+            autoEscolaService.getAll().then(
+                function(data){
+                    self.autoEscolas = data;
+                },function(reason){
+                    console.log(reason);
+                });
+        };
+
+
     };
 }());
