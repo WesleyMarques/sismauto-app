54ee84d1915fe598e9ffb9d91105f2613d029e18
Isabelly Cavalcante <isabelly.cavalcante@ccc.ufcg.edu.br>
2016-04-27 22:30:57 -0300

    alterando a view de aulas

--- sistemauto/www/js/app.js
+++ sistemauto/www/js/app.js
@@ -21,9 +21,8 @@
             if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
                 cordova.plugins.Keyboard.disableScroll(true);
-
-
             }
+
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
@@ -80,10 +79,6 @@
                 },
                 params: {'autoEscola': undefined}
             });
-
-
         $urlRouterProvider.otherwise('/home');
     });
-
-
 })();
